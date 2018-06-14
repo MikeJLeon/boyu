18185d084c902738abc024a8017aded139943d23
MikeJLeon <michaeljleon@live.com>
2018-06-13 23:04:52 -0700

    wordpress uploaded

---
+++ wp-content/themes/design/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Boyu Zhang</title>
+	<meta charset="utf-8">
+
+	
+    <!-- I'm honestly not sure if bootstrap even uses jquery but the tutorial said to put this here -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
+
+
+    <!-- load bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="wp-content/themes/design/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="container">
+                
+                <div class="row">
+                    <div class="col-md-12">
+                    </div>
+                </div>
+                <?php
+                $count = 0; 
+                while(have_posts()) : the_post();
+                    if($count == 0){?>
+                        <div class="row">
+                            <a data-toggle="modal" data-target="#imgModal<?php echo get_the_ID()?>" style="z-index:10">
+                                <div class="col-md-4">
+                                    <img class="featured" src="<?php echo the_post_thumbnail_url(" large "); ?>">
+                                </div> 
+                            </a>
+                            <div class="modal fade allModal" id="imgModal<?php echo get_the_ID()?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            <span class="close" name="betaClose" data-dismiss="modal">&times;</span>
+                                                <img src="<?php echo the_post_thumbnail_url(" large "); ?>">
+                                                <h2>Hello</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    <?php $count++;
+                    }else if($count % 2 == 0){
+                        ?>
+                        <a data-toggle="modal" data-target="#imgModal<?php echo get_the_ID()?>" style="z-index:10">
+                                <div class="col-md-4">
+                                    <img class="featured" src="<?php echo the_post_thumbnail_url(" large "); ?>">
+                                </div> 
+                            </a>
+                            <div class="modal fade allModal" id="imgModal<?php echo get_the_ID()?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            <span class="close" name="betaClose" data-dismiss="modal">&times;</span>
+                                                <img src="<?php echo the_post_thumbnail_url(" large "); ?>">
+                                                <h2>Hello</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                        $count = 0;
+                    }else{
+                        ?>
+                        <a data-toggle="modal" data-target="#imgModal<?php echo get_the_ID()?>" style="z-index:10">
+                                <div class="col-md-4">
+                                    <img class="featured" src="<?php echo the_post_thumbnail_url(" large "); ?>">
+                                </div> 
+                            </a>
+                            <div class="modal fade allModal" id="imgModal<?php echo get_the_ID()?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            <span class="close" name="betaClose" data-dismiss="modal">&times;</span>
+                                                <img src="<?php echo the_post_thumbnail_url(" large "); ?>">
+                                                <h2>Hello</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php
+                        $count++;
+                    }
+                endwhile;
+                    ?>
+        </div>
+    </div>
+    </div>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</body>
+
+</html>
---
+++ wp-content/themes/home/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Boyu Zhang</title>
+	<meta charset="utf-8">
+
+	
+    <!-- I'm honestly not sure if bootstrap even uses jquery but the tutorial said to put this here -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
+
+
+    <!-- load bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="wp-content/themes/home/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <a href="design">
+                        <div class="col-md-12">
+                        <div class="white">
+                            <div class="front">
+                                <h3>Design</h3>
+                            </div>
+                            <div class="back">
+                                <p>BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH</p>    
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="photography">
+                        <div class="col-md-12">
+                            <div class="white">
+                                <div class="front">
+                                    <h3>Photography</h3>
+                                </div>
+                                <div class="back">
+                                    <p>BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="podcasts">
+                        <div class="col-md-12">
+                            <div class="white">
+                                <div class="front">
+                                    <h3>Podcasts</h3>
+                                </div>
+                                <div class="back">
+                                <p>BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="journal">
+                        <div class="col-md-12">
+                            <div class="white">
+                                <div class="front">
+                                    <h3>Journal</h3>
+                                </div>
+                                <div class="back">
+                                    <p>BLAHBLAH BLAH BLAH BLAH BLAH BLAHBLAH</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div id="square">
+                    <h3>Boyu Zhang</h3>
+                    <p>I'm currently a graphic design student trying to start my career</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</body>
+
+</html>
